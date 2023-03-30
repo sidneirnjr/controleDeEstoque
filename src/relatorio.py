f24032f69888d317c46b9cad1084a9592c79fb4f
sidneirnjr <sidneirnjr@live.com>
2023-03-29 23:19:20 -0300

    1

--- src/relatorio.py
+++ src/relatorio.py
@@ -1,3 +1,6 @@
+import tkinter as tk
+from tkinter import ttk
+
 class Relatorio:
     def __init__(self, vendas: List[Venda]):
         self.vendas = vendas
@@ -27,3 +30,33 @@ class Relatorio:
                 vendas_periodo.append(venda)
 
         return vendas_periodo
+
+
+class RelatorioInterface:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Relatório")
+
+        # Criação do frame
+        main_frame = ttk.Frame(self.root, padding=20)
+        main_frame.pack(fill=tk.BOTH, expand=True)
+
+        # Label do título
+        titulo_label = ttk.Label(main_frame, text="Relatório")
+        titulo_label.pack(pady=10)
+
+        # Label para selecionar o período do relatório
+        periodo_label = ttk.Label(main_frame, text="Selecione o período do relatório:")
+        periodo_label.pack(pady=10)
+
+        # Criação do combobox para selecionar o período
+        periodo_combobox = ttk.Combobox(main_frame, values=["Diário", "Semanal", "Mensal"])
+        periodo_combobox.pack()
+
+        # Botão para gerar o relatório
+        gerar_relatorio_button = ttk.Button(main_frame, text="Gerar Relatório")
+        gerar_relatorio_button.pack(pady=20)
+
+        # Botão para fechar a janela
+        fechar_button = ttk.Button(main_frame, text="Fechar", command=self.root.destroy)
+        fechar_button.pack(pady=10)
