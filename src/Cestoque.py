8e82857a69173394bf33b70c43247ee78cec1325
sidneirnjr <sidneirnjr@live.com>
2023-03-30 00:09:20 -0300

    2

---
+++ src/Cestoque.py
@@ -0,0 +1,32 @@
+class Estoque:
+    def __init__(self):
+        self.produtos = []
+
+    def adicionar_produto(self, produto):
+        self.produtos.append(produto)
+
+    def remover_produto(self, codigo):
+        produto = self.pesquisar_por_palavra_chave(codigo)
+        if produto is not None:
+            self.produtos.remove(produto)
+            print("Produto removido com sucesso!")
+        else:
+            print("Produto não encontrado.")
+
+    def alterar_quantidade(self, codigo, quantidade):
+        produto = self.pesquisar_por_palavra_chave(codigo)
+        if produto is not None:
+            produto.set_quantidade(quantidade)
+            print("Quantidade alterada com sucesso!")
+        else:
+            print("Produto não encontrado.")
+
+    def listar_produtos(self):
+        for produto in self.produtos:
+            produto.imprimir()
+
+    def pesquisar_por_palavra_chave(self, palavra_chave):
+        for produto in self.produtos:
+            if palavra_chave.lower() in produto.get_nome().lower() or palavra_chave.lower() in produto.get_descricao().lower():
+                return produto
+        return None
--- src/estoque.py
+++ src/estoque.py
@@ -1,40 +1,6 @@
 from tkinter import *
 from tkinter import messagebox
-from estoque import Estoque
-
-
-class Estoque:
-    def __init__(self):
-        self.produtos = []
-
-    def adicionar_produto(self, produto):
-        self.produtos.append(produto)
-
-    def remover_produto(self, codigo):
-        produto = self.pesquisar_por_palavra_chave(codigo)
-        if produto is not None:
-            self.produtos.remove(produto)
-            print("Produto removido com sucesso!")
-        else:
-            print("Produto não encontrado.")
-
-    def alterar_quantidade(self, codigo, quantidade):
-        produto = self.pesquisar_por_palavra_chave(codigo)
-        if produto is not None:
-            produto.set_quantidade(quantidade)
-            print("Quantidade alterada com sucesso!")
-        else:
-            print("Produto não encontrado.")
-
-    def listar_produtos(self):
-        for produto in self.produtos:
-            produto.imprimir()
-
-    def pesquisar_por_palavra_chave(self, palavra_chave):
-        for produto in self.produtos:
-            if palavra_chave.lower() in produto.get_nome().lower() or palavra_chave.lower() in produto.get_descricao().lower():
-                return produto
-        return None
+from Cestoque import Estoque
 
 
 class EstoqueInterface:
